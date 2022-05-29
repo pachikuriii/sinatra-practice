754188da865601c6a340da56d8955a9b366c334e
pachikuriii <stakahashi0728@gmail.com>
2022-05-29 09:29:36 +0900

    各詳細ページへのルーティング完了

--- app.rb
+++ app.rb
@@ -5,19 +5,12 @@ require 'json'
 
 
 get '/' do 
-@display_title = ""
-    File.open("db.json", 'r') do |file|
-      ruby_hash = File.open("db.json", 'r') { |json| JSON.load(json) } 
-      hashs = ruby_hash.map {|hash| hash}
-
-       hashs.each do |hash|
-        @display_title = @display_title + "<li>"
-        @display_title = @display_title + "<a href=\"memos/#{hash["id"]}\">"
-        @display_title = @display_title + "#{hash["title"]}"
-        @display_title = @display_title + "</a>"
-        @display_title = @display_title + "</li>\n"
+      files = Dir.glob("memos/*")
+      @memos = files.map do |file|
+         JSON.load(File.open("#{file}"))
       end
-    end
+
+      
 erb :index
 end
 
@@ -29,24 +22,27 @@ post '/memos' do
   @title = params["title"]
   @content = params["content"]
 
-    data = File.open("db.json", 'r') { |json| JSON.load(json) } 
-    ids = []
-    data.each do|hash|
-       ids << "#{hash["id"]}"
-    end
-    max = ids.max.to_i
-
-    new_memo = { "id": "#{max + 1}", "title": @title, "content": @content }
-    data << new_memo
-    File.open("db.json", 'w') { |json| JSON.dump(data, json)}
+    hash = { "id": SecureRandom.uuid, "title": @title, "content": @content }
+    File.open("memos/#{hash[:id]}.json", 'w') { |file| file.puts JSON.generate(hash) }  
    
   redirect '/'
 end
 
-get '/memos/*' do |id|
+get '/memos/:id' do
+   file = JSON.load(File.open("memos/#{params["id"]}.json"))
+   @title = file["title"]
+   @content = file["content"]
+  erb :memos_show
+end
 
+
+
+
+get '/memos/*/edit' do |id|
   
-  erb :memos_new
+  @title = titles
+  @content = contents
+  erb :memos_edit
 end
 
 
