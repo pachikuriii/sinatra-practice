5f3204e77dafce2179a10ee78d7f5117c670ac8e
pachikuriii <stakahashi0728@gmail.com>
2022-05-29 20:39:52 +0900

    一旦全機能を網羅

--- app.rb
+++ app.rb
@@ -2,22 +2,11 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
 
-
-
 get '/' do 
-@display_title = ""
-    File.open("db.json", 'r') do |file|
-      ruby_hash = File.open("db.json", 'r') { |json| JSON.load(json) } 
-      hashs = ruby_hash.map {|hash| hash}
-
-       hashs.each do |hash|
-        @display_title = @display_title + "<li>"
-        @display_title = @display_title + "<a href=\"memos/#{hash["id"]}\">"
-        @display_title = @display_title + "#{hash["title"]}"
-        @display_title = @display_title + "</a>"
-        @display_title = @display_title + "</li>\n"
+      files = Dir.glob("memos/*")
+      @memos = files.map do |file|
+         JSON.load(File.open("#{file}"))
       end
-    end
 erb :index
 end
 
@@ -26,28 +15,36 @@ get '/memos/new' do
 end
 
 post '/memos' do
-  @title = params["title"]
-  @content = params["content"]
-
-    data = File.open("db.json", 'r') { |json| JSON.load(json) } 
-    ids = []
-    data.each do|hash|
-       ids << "#{hash["id"]}"
-    end
-    max = ids.max.to_i
-
-    new_memo = { "id": "#{max + 1}", "title": @title, "content": @content }
-    data << new_memo
-    File.open("db.json", 'w') { |json| JSON.dump(data, json)}
+    hash = { "id": SecureRandom.uuid, "title": params["title"], "content": params["content"] }
+    File.open("memos/#{hash[:id]}.json", 'w') { |file| file.puts JSON.generate(hash) }  
    
+    redirect '/'
+end
+
+get '/memos/:id' do
+   file = JSON.load(File.open("memos/#{params["id"]}.json"))
+   @title = file["title"]
+   @content = file["content"]
+  erb :memos_show
+end
+
+get '/memos/:id/edit' do
+  file = JSON.load(File.open("memos/#{params["id"]}.json"))
+  @title = file["title"]
+  @content = file["content"]
+  erb :memos_edit
+end
+
+patch '/memos/:id' do
+  File.open("memos/#{params["id"]}.json", 'w') do |file|
+  hash = {"id": params["id"], "title": params["title"], "content": params["content"] }
+  JSON.dump(hash, file)
+  end
+  
   redirect '/'
 end
 
-get '/memos/*' do |id|
-
-  
-  erb :memos_new
+delete '/memos/:id' do
+  File.delete("memos/#{params["id"]}.json")
+  redirect '/'
 end
-
-
-
