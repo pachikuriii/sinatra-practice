6e0ffe32ce3d503d3979f61b431c5482c2120df0
pachikuriii <stakahashi0728@gmail.com>
2022-06-08 12:19:39 +0900

    HTMLエスケープ処理をview側で行うように変更

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ get '/memos/new' do
 end
 
 post '/memos' do
-  hash = { "id": SecureRandom.uuid, "title": CGI.escapeHTML(params['title']), "content": CGI.escapeHTML(params['content']) }
+  hash = { "id": SecureRandom.uuid, "title": params['title'], "content": params['content'] }
   File.open("memos/#{hash[:id]}.json", 'w') { |file| file.puts JSON.generate(hash) }
   redirect '/'
 end
@@ -39,7 +39,7 @@ end
 
 patch '/memos/:id' do
   File.open("memos/#{params['id']}.json", 'w') do |file|
-    hash = { "id": params['id'], "title": CGI.escapeHTML(params['title']), "content": CGI.escapeHTML(params['content']) }
+    hash = { "id": params['id'], "title": params['title'], "content": params['content'] }
     JSON.dump(hash, file)
   end
   redirect "/memos/#{params['id']}"
